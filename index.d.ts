4de89c813e80ae9e52a64e5321562b9d72efb04c
Jason Liang <liangjyjason@gmail.com>
2019-12-19 23:44:37 -0500

    add presentReporter to index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,13 @@
 declare module "react-native-buglife" {
-  const invocationOptionsNone = 'invocationOptionsNone';
-  const invocationOptionsShake = 'invocationOptionsShake';
-  const invocationOptionsScreenshot = 'invocationOptionsScreenshot';
-  const invocationOptionsFloatingButton = 'invocationOptionsFloatingButton';
-  const BuglifeAttachmentRequest = 'BuglifeAttachmentRequest';
+  const invocationOptionsNone = "invocationOptionsNone"
+  const invocationOptionsShake = "invocationOptionsShake"
+  const invocationOptionsScreenshot = "invocationOptionsScreenshot"
+  const invocationOptionsFloatingButton = "invocationOptionsFloatingButton"
+  const BuglifeAttachmentRequest = "BuglifeAttachmentRequest"
 
-  function setInvocationOptions(options: string): void;
-  function startWithAPIKey(key: string): void;
-  function setUserIdentifier(id: string): void;
-  function setUserEmail(email: string): void;
+  function setInvocationOptions(options: string): void
+  function startWithAPIKey(key: string): void
+  function setUserIdentifier(id: string): void
+  function setUserEmail(email: string): void
+  function presentReporter(): void
 }
